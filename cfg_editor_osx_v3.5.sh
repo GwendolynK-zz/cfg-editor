2deab1a42fa71d5d4223492f800bf88d948aba64
Westley Rose <westleyrose@westleys-mbp.attlocal.net>
2018-04-27 12:44:36 -0700

    added osx script

---
+++ cfg_editor_osx_v3.5.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# created by Westley
+# email westley@sylabs.io
+# date code starded 1751
+# date sterded December 7, 2017
+# updated on 1817
+# date April 27, 2018
+# version 3.5
+
+
+# check where this script is
+where=$( pwd | rev | cut -c1-5 | rev )
+if [ "${where/arts}" = "$where" ]; then
+	echo "this should be in your parts folder"
+	exit
+fi
+
+
+echo
+echo "WARNING this will add self destruct line to every .cfg file in its directory"
+echo "this shell script should be in a parts folder"
+echo
+echo "are you sure you want to do this?"
+echo "[y,n]"
+
+read input1
+if [[ $input1 == "y" || $input1 == "Y" ]]; then
+
+	echo "Loading..."
+	# its a verry cheap way of doing this, but it works :)
+	file_counter=1
+	max_files=$( find . | grep '.cfg$' | cut -c3- | sort | wc -l )
+	# give some time to make it feel like its good quality :)
+	sleep 2s
+	if [[ "$max_files" -eq "0" ]]; then 
+		echo 
+		echo "theres no .cfg files to edit. make sure this is in your parts folder"
+		exit
+	fi
+	find . | grep '.cfg$' | cut -c3- | sort
+	echo
+	echo "$max_files files"
+	echo
+	echo "dose this look right?"
+	echo "[y,n]"
+	read input2
+	if [[ $input2 == "y" || $input2 == "Y" ]]; then
+
+		echo
+		echo "Loading..."
+		# some extra time here to!
+		sleep 2.5s
+		for (( e=1; e <= $max_files; e++ )) do
+			# notice were do'in a find every time its writing to a .cfg file, not ideal
+			file_location=$( find . | grep '.cfg$' | head -$file_counter | tail -1 | cut -c3- )
+			# check if the module allready exist
+			mod_check=$( cat $file_location | grep 'TacSelfDestruct' )
+			echo $file_counter
+			echo $file_location
+			echo
+			if [ -z "$mod_check" ]; then
+				# open the last bracket
+				tail -r $file_location > cfg_file.txt
+				#sed -i '0,/\}/s//\/\/\}/' cfg_file.txt
+				sed -i .bak 's/\}/\/\/\}/' cfg_file.txt
+				tail -r cfg_file.txt > $file_location
+				# then echo the module to the file, notice the extra bracket
+				echo "
+    }	
+	MODULE {
+	  name = TacSelfDestruct
+	  timeDelay = 10.0
+	  canStage = false
+  }
+}
+
+  " >> $file_location
+  # you can modify this as you need, eg. "canStage = true" insted of false
+  		((file_counter ++))
+		else
+			echo "  ^already has module"
+			echo
+			((file_counter ++))
+		fi
+	done
+
+	echo
+	rm cfg_file.txt
+	rm cfg_file.txt.bak
+	echo "done"
+else 
+	echo "okay"
+	exit
+fi
+
+else
+	echo "your loss"
+	exit
+fi
+
+
+#
+# ChangeLog:
+#
+
+# --version_3.5-- cfg_editor_v3.5.sh
+# commets the last bracket and then adds the module
+# better than the previous version
+
+# --version_3.4-- cfg_editor_v3.4.sh
+# now removes the last bracket before adding module
+
+# --version_3.3-- cfg_editor_v3.3.sh
+# now detects where it is and wont run if its not in a parts folder
+
+# --version_3.2-- cfg_editor_v3.2.sh
+# added no cfg file exit
+
+# --version_3.1-- cfg_editor_v3.1.sh
+#  first public release
+
+
+
+# End ChangeLog
+
+
